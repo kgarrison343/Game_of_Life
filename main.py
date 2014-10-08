92c2a1439f1423680226d12d7c10a3a398b03ce4
Kyle Garrison <kgarrison343@gmail.com>
2014-10-07 23:25:24 -0400

    Added infinite loop to and stepLogic method to complete game of life logic.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # main.py
 import neighborLogic
 
+START_GRID = [[0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0],
+              [0,0,1,1,1,0,0,0,0,0],
+              [0,0,0,0,1,0,0,0,0,0],
+              [0,0,0,1,0,0,0,0,0,0]]
+
 def display2DGrid(grid):
   "Print 2D grid to screen"
   for row in range(len(grid)):
@@ -9,17 +20,22 @@ def display2DGrid(grid):
 			
     print()
 
+def stepLogic(mainGrid, neighborGrid):
+  for x, row in enumerate(neighborGrid):
+    for y, cell in enumerate(row):
+      if cell == 3:
+        mainGrid[x][y] = 1
+      elif cell < 2 or cell > 3:
+        mainGrid[x][y] = 0
+  return mainGrid
+        
+
 if __name__ == "__main__":
-    mainGrid = []
-	
-    for row in range(10):
-      # Fill grid with lists
-      mainGrid.append([])
-      for column in range(10):
-        # Fill grid with 1s
-        mainGrid[row].append(1)
-	
-    neighborGrid = neighborLogic.checkNumOfNeighbors(mainGrid)
-    display2DGrid(mainGrid)
-    input('Press enter to continue...')
-    display2DGrid(neighborGrid)
+
+    mainGrid = START_GRID
+
+    while(1):
+      neighborGrid = neighborLogic.checkNumOfNeighbors(mainGrid)
+      display2DGrid(mainGrid)
+      input('Press enter to continue...')
+      mainGrid = stepLogic(mainGrid, neighborGrid)
