0139c60a4599c80ce692e99a70e9fe84c589f18f
Kyle Garrison <kgarrison343@gmail.com>
2014-10-07 23:00:12 -0400

    Fixed bug causing incorrect neighbor reporting and fixed formating errors.

--- main.py
+++ main.py
@@ -2,24 +2,24 @@
 import neighborLogic
 
 def display2DGrid(grid):
-	"Print 2D grid to screen"
-	for row in range(len(grid)):
-		for column in range(len(grid[row])):
-			print(grid[row][column], end=' ')
+  "Print 2D grid to screen"
+  for row in range(len(grid)):
+    for column in range(len(grid[row])):
+      print(grid[row][column], end=' ')
 			
-		print("\n")
+    print()
 
 if __name__ == "__main__":
-	mainGrid = []
+    mainGrid = []
 	
-	for row in range(10):
-		# Fill grid with lists
-		mainGrid.append([])
-		for column in range(10):
-			# Fill grid with 1s
-			mainGrid[row].append(1)
-			
-	neighborGrid = neighborLogic.checkNumOfNeighbors(mainGrid)
-	display2DGrid(mainGrid)
-	input('Press enter to continue...')
-	display2DGrid(neighborGrid)
+    for row in range(10):
+      # Fill grid with lists
+      mainGrid.append([])
+      for column in range(10):
+        # Fill grid with 1s
+        mainGrid[row].append(1)
+	
+    neighborGrid = neighborLogic.checkNumOfNeighbors(mainGrid)
+    display2DGrid(mainGrid)
+    input('Press enter to continue...')
+    display2DGrid(neighborGrid)
--- neighborLogic.py
+++ neighborLogic.py
@@ -1,18 +1,18 @@
 def checkNumOfNeighbors(grid):
-	neighborGrid = []
-	H = len(grid)
-	for x in range(H):
-		neighborGrid.append([])
-		W = len(grid[x])
-		for y in range(W):
-			neighborGrid[x].append(0)
-			for i in range(-1, 2):
-				for j in range(-1, 2):
-					if i != 0 and j != 0:
-						rx = x + i
-						ry = y + j
-						
-						if rx >= 0 and ry >= 0 and rx < W and ry < H:
-							if grid[rx][ry] == 1:
-								neighborGrid[x][y] += 1	
-	return neighborGrid
+  neighborGrid = []
+  H = len(grid)
+  for x in range(H):
+    neighborGrid.append([])
+    W = len(grid[x])
+    for y in range(W):
+      neighborGrid[x].append(0)
+      for i in range(-1, 2):
+        for j in range(-1, 2):
+          if i != 0 or j != 0:
+            rx = x + i
+            ry = y + j
+
+            if rx >= 0 and ry >= 0 and rx < W and ry < H:
+              if grid[rx][ry] == 1:
+                neighborGrid[x][y] += 1
+  return neighborGrid
