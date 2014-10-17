aab666a43485c5542d29b4b3590f6d009dda0b0e
Kyle Garrison <kgarrison343@gmail.com>
2014-10-16 22:10:10 -0400

    Implement pygame graphics and event handling

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # main.py
-import neighborLogic
+
+import sys
+
+import pygame
+from pygame.locals import *
+
+from neighborLogic import num_of_neighbors
+
 
 START_GRID = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -11,7 +18,6 @@ START_GRID = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
               [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
-
 
 def display_2d_grid(grid):
     """Print 2D grid to screen"""
@@ -38,12 +44,51 @@ def step_logic(main_grid, neighbor_grid):
     return main_grid
 
 
+def display_2D_grid_pygame(grid):
+    """
+
+    :param grid:
+    """
+    cell_length = (windowLength // 10) - 2
+
+    windowSurfaceObj.fill(grey)
+
+    cell_x, cell_y = 2, 2
+    for row in mainGrid:
+        for cell in row:
+            if cell == 1:
+                pygame.draw.rect(windowSurfaceObj, black, (cell_x, cell_y, cell_length, cell_length))
+            else:
+                pygame.draw.rect(windowSurfaceObj, white, (cell_x, cell_y, cell_length, cell_length))
+            cell_x += cell_length + 2
+        cell_y += cell_length + 2
+        cell_x = 2
+
 if __name__ == "__main__":
+    pygame.init()
+    fps_clock = pygame.time.Clock()
+
+    windowLength = 302
+    windowSurfaceObj = pygame.display.set_mode((windowLength, windowLength))
+    pygame.display.set_caption('Game of Life')
+
+    white = pygame.Color(255, 255, 255)
+    black = pygame.Color(0, 0, 0)
+    grey = pygame.Color(127, 127, 127)
 
     mainGrid = START_GRID
-    cease = 'y'
-    while cease != 'n':
-        neighborGrid = neighborLogic.num_of_neighbors(mainGrid)
-        display_2d_grid(mainGrid)
-        cease = input('Continue? (Y/N)')
-        mainGrid = step_logic(mainGrid, neighborGrid)
+
+    display_2D_grid_pygame(mainGrid)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    neighborGrid = num_of_neighbors(mainGrid)
+                    step_logic(mainGrid, neighborGrid)
+                    display_2D_grid_pygame(mainGrid)
+
+        pygame.display.update()
+        fps_clock.tick(30)
